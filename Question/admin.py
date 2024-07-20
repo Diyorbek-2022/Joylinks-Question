4145cd1b08ec8808f964ca19c800e8c04077b11a
Diyorbek-2022 <diyorbek5896@gmail.com>
2024-07-20 16:16:47 +0500

    commit

--- Question/admin.py
+++ Question/admin.py
@@ -6,9 +6,9 @@ from .models import User
 # Register your models here.
 
 
-@admin.register(User)
-class register_user(admin.ModelAdmin):
-    list_display = ['user_id', 'full_name', 'email', 'score']
-    ordering = ['-correct_answer', '-created_time']
-    search_fields = ['first_name']
-    list_filter = ['score', 'created_time']
+admin.site.register(User)
+# class register_user(admin.ModelAdmin):
+#     list_display = ['user_id', 'full_name', 'email', 'score']
+#     ordering = ['-correct_answer', '-created_time']
+#     search_fields = ['first_name']
+#     list_filter = ['score', 'created_time']
